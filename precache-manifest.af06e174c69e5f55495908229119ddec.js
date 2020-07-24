b1f105b684a93132bc2fa7229b6f7603f448bf57
Daniel Ilich <ukqfhf@yandex.ru>
2020-07-24 11:38:43 +0300

    Updates

---
+++ precache-manifest.af06e174c69e5f55495908229119ddec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ec7d7472ed7633ff22134e70f67cee",
+    "url": "/future-test/index.html"
+  },
+  {
+    "revision": "8b095d5869e7e8f2bb88",
+    "url": "/future-test/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "f01d2a8a3ed27d077b4e",
+    "url": "/future-test/static/js/2.ba7c6179.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/future-test/static/js/2.ba7c6179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b095d5869e7e8f2bb88",
+    "url": "/future-test/static/js/main.c226005c.chunk.js"
+  },
+  {
+    "revision": "36c72122a9bf6eafc802",
+    "url": "/future-test/static/js/runtime-main.232c2893.js"
+  }
+]);
